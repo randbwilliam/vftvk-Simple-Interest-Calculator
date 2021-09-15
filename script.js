b98d1c011e6080bd3e635a7faf6c5361b57510f2
randbwilliam <72697907+randbwilliam@users.noreply.github.com>
2021-09-15 12:31:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //Get values and calculate
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest =principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() +years;
     
+    //Create Interest text
+    document.getElementById("result").innerHTML = "Interest : If you despoit <mark>" + principal + "</mark>,<br/>" +
+        "at an interest rate of <mark> + rate + </mark>,<br/> +
+        "YOu will receive an amount of <mark>" + interest + "</mark>,<br/>" +
+        " in the year <mark>" yearInTheFuture + "</mark>";
 }
         
